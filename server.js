a42d9edb494554053765f2ede10a5a98f3786566
Santiago Hincapie <santiagohr81@hotmail.com>
2020-11-23 16:54:54 -0500

    added a sever, notes, api, and view js files

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 // Dependencies
 
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+// const api = require("./routes/api");
+const view = require("./routes/view");
 
-// Set up the Express App
+//  Set up the Express App
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+// middleware
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use("/api",api);
+app.use("/",view)
+
+
+
+
+app.listen(PORT,()=>console.log("listing on "+PORT));
+
