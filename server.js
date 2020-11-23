b5e429fd3c80088ac89f1378205f7bb857643770
Santiago Hincapie <santiagohr81@hotmail.com>
2020-11-23 14:47:33 -0500

    first setup

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Set up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000
