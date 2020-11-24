a5e9ffbe86858c45543ce31af861438033bc6203
Santiago Hincapie <santiagohr81@hotmail.com>
2020-11-24 17:46:03 -0500

    started api

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,14 @@
-//req.body 
+// load Data
+const db = require("../db/db.json");
+
+
+// Routing
+
+module.exports =function(app){
+
+app.get("/api/notes", function(req, res){
+res.json(db)
+});
+
+};
+
