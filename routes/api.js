c0ec2369a3fa2f418c9498022568b8c0008c4462
Santiago Hincapie <santiagohr81@hotmail.com>
2020-12-01 18:39:18 -0500

    api update

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,27 @@
 // load Data
-const db = require("../db/db.json");
+const notes = require("../db/notes");
+const router = require("express").Router()
 
-
-// Routing
-
-module.exports =function(app){
-
-app.get("/api/notes", function(req, res){
-res.json(db)
+router.get("/notes", function(req, res) {
+  notes
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
 });
 
-};
+router.post("/notes", (req, res) => {
+  notes
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
 
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", function(req, res) {
+  notes
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 
 const express = require("express");
-// const api = require("./routes/api");
+const api = require("./routes/api");
 const view = require("./routes/view");
 
 //  Set up the Express App
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use("/api",api);
+app.use("/api",api);
 app.use("/",view)
 
 
